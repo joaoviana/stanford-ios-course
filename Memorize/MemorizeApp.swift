fdc85ee12ca5783ec29a1a05c7fe7634fd2f6a00
joao viana <joao.viana@beamery.com>
2020-12-28 16:57:04 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by João Viana on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
